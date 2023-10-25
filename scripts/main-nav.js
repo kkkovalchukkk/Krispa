c1bb92afea20bbd5a2fcf885d2bcd3e040736838
Egor <egorrachkov0@gmail.com>
2023-10-26 00:32:19 +0500

    401 error fix

--- scripts/main-nav.js
+++ scripts/main-nav.js
@@ -1,55 +1,63 @@
 window.addEventListener('DOMContentLoaded', () => {
   const mainNavListItemSubmenu = document.querySelectorAll(
     '.main-nav-list-item--submenu'
-  );
+  )
 
-  const mainNav = document.querySelector('.main-nav');
+  const mainNav = document.querySelector('.main-nav')
+
+  // const mobileMenuNav = document.querySelector('.mobile-menu__nav-block--submenu')
+  // const mobileMenuNavSubItems = document.querySelectorAll('.main-nav-list-item__submenu-col-heading')
+
 
   let previousPosition =
-    window.pageYOffset || document.documentElement.scrollTop;
+    window.pageYOffset || document.documentElement.scrollTop
 
   window.onscroll = function () {
     let currentPosition =
-      window.pageYOffset || document.documentElement.scrollTop;
-    console.log(currentPosition);
+      window.pageYOffset || document.documentElement.scrollTop
+    console.log(currentPosition)
 
     if (currentPosition >= 130) {
-      document.querySelector('.header').classList.add('fixed');
-      document.querySelector('.navigation-section').classList.add('fixed');
+      document.querySelector('.header').classList.add('fixed')
+      document.querySelector('.navigation-section').classList.add('fixed')
     } else {
-      document.querySelector('.header').classList.remove('fixed');
-      document.querySelector('.navigation-section').classList.remove('fixed');
+      document.querySelector('.header').classList.remove('fixed')
+      document.querySelector('.navigation-section').classList.remove('fixed')
     }
-    previousPosition = currentPosition;
-  };
+    previousPosition = currentPosition
+  }
+
+  // mobileMenuNav.addEventListener('click', () => {
+  //   mobileMenuNav.classList.add('main-nav-list-item--active')
+  // })
 
   mainNavListItemSubmenu.forEach((item) => {
     item.addEventListener('mouseenter', () => {
-      item.classList.add('main-nav-list-item--active');
-      mainNav.classList.add('main-nav--non-rounded');
-      const subMenu = item.querySelector('.main-nav-list-item__submenu');
-      subMenu.style.maxHeight = subMenu.scrollHeight + 'px';
-    });
+      item.classList.add('main-nav-list-item--active')
+      mainNav.classList.add('main-nav--non-rounded')
+      const subMenu = item.querySelector('.main-nav-list-item__submenu')
+      subMenu.style.maxHeight = subMenu.scrollHeight + 'px'
+    })
 
     item.addEventListener('mouseleave', () => {
-      mainNav.classList.remove('main-nav--non-rounded');
-      const subMenu = item.querySelector('.main-nav-list-item__submenu');
-      item.classList.remove('main-nav-list-item--active');
-      subMenu.style.maxHeight = '0px';
-    });
+      mainNav.classList.remove('main-nav--non-rounded')
+      const subMenu = item.querySelector('.main-nav-list-item__submenu')
+      item.classList.remove('main-nav-list-item--active')
+      subMenu.style.maxHeight = '0px'
+    })
 
     item.addEventListener('click', () => {
       if (item.classList.contains('main-nav-list-item--active')) {
-        mainNav.classList.remove('main-nav--non-rounded');
-        const subMenu = item.querySelector('.main-nav-list-item__submenu');
-        item.classList.remove('main-nav-list-item--active');
-        subMenu.style.maxHeight = '0px';
+        mainNav.classList.remove('main-nav--non-rounded')
+        const subMenu = item.querySelector('.main-nav-list-item__submenu')
+        item.classList.remove('main-nav-list-item--active')
+        subMenu.style.maxHeight = '0px'
       } else {
-        item.classList.add('main-nav-list-item--active');
-        mainNav.classList.add('main-nav--non-rounded');
-        const subMenu = item.querySelector('.main-nav-list-item__submenu');
-        subMenu.style.maxHeight = subMenu.scrollHeight + 'px';
+        item.classList.add('main-nav-list-item--active')
+        mainNav.classList.add('main-nav--non-rounded')
+        const subMenu = item.querySelector('.main-nav-list-item__submenu')
+        subMenu.style.maxHeight = subMenu.scrollHeight + 'px'
       }
-    });
-  });
-});
+    })
+  })
+})
