f0ff4974c644ac867e88bdb4024c28a0780e516e
Egor <egorrachkov0@gmail.com>
2023-10-27 00:36:44 +0500

    394 error fixed

--- scripts/main-nav.js
+++ scripts/main-nav.js
@@ -1,16 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const mainNavListItemSubmenu = document.querySelectorAll(
-    '.main-nav-list-item--submenu'
-  )
-
+  const mainNavListItemSubmenu = document.querySelectorAll('.main-nav-list-item--submenu')
   const mainNav = document.querySelector('.main-nav')
-
-  // const mobileMenuNav = document.querySelector('.mobile-menu__nav-block--submenu')
-  // const mobileMenuNavSubItems = document.querySelectorAll('.main-nav-list-item__submenu-col-heading')
-
 
   let previousPosition =
     window.pageYOffset || document.documentElement.scrollTop
+
+  const mobileMenu = document.querySelector('.mobile-menu__nav')
+  const mobileNavBtn = document.querySelector('.mobile-menu__nav-block--submenu--btn')
+  const mobileNavBtnInner = document.querySelector('.mobile-menu__nav-block--submenu--nonDisplayed')
+  const mobileNavBtnInnerBlocks = document.querySelectorAll('.mobile-menu__nav-block--submenu__li')
+  const mobileBlocksInnerContent = document.querySelectorAll('.mobile-menu__nav-block--submenu__li-nav')
+  // const mobileBlocksInnerContentShowed = document.querySelectorAll('.mobile-menu__nav-block--submenu__li-nav-showedContent')
+  const mobileInnerContentBtns = document.querySelectorAll('.mobile-menu__nav-block--submenu-btn')
 
   window.onscroll = function () {
     let currentPosition =
@@ -27,9 +28,43 @@ window.addEventListener('DOMContentLoaded', () => {
     previousPosition = currentPosition
   }
 
-  // mobileMenuNav.addEventListener('click', () => {
-  //   mobileMenuNav.classList.add('main-nav-list-item--active')
-  // })
+  mobileNavBtn.addEventListener('click', () => {
+    mobileMenu.style.visibility = 'hidden'
+    mobileMenu.style.opacity = 0
+    mobileMenu.style.marginBottom = '42px'
+    mobileNavBtnInner.classList.toggle('mobile-menu__nav-block--submenu--nonDisplayed')
+  })
+
+  mobileInnerContentBtns.forEach((btn, index) => {
+    const svg = btn.querySelector('svg')
+
+    btn.addEventListener('click', () => {
+      mobileBlocksInnerContent.forEach((content, contentIndex) => {
+        if (contentIndex === index) {
+          if (content.classList.contains('mobile-menu__nav-block--submenu__li-nav-showedContent')) {
+            content.classList.remove('mobile-menu__nav-block--submenu__li-nav-showedContent')
+            content.classList.add('mobile-menu__nav-block--submenu__li-nav')
+            mobileMenu.style.marginBottom = '42px'
+            if (svg) {
+              svg.style.transform = 'rotate(0deg)'
+              svg.style.transition = 'transform 0.23s'
+            }
+          } else {
+            content.classList.add('mobile-menu__nav-block--submenu__li-nav-showedContent')
+            content.classList.remove('mobile-menu__nav-block--submenu__li-nav')
+            mobileMenu.style.marginBottom = '-262px'
+            if (svg) {
+              svg.style.transform = 'rotate(180deg)'
+              svg.style.transition = 'transform 0.23s'
+            }
+          }
+        } else {
+          content.classList.remove('mobile-menu__nav-block--submenu__li-nav-showedContent')
+          content.classList.add('mobile-menu__nav-block--submenu__li-nav')
+        }
+      })
+    })
+  })
 
   mainNavListItemSubmenu.forEach((item) => {
     item.addEventListener('mouseenter', () => {
@@ -38,6 +73,10 @@ window.addEventListener('DOMContentLoaded', () => {
       const subMenu = item.querySelector('.main-nav-list-item__submenu')
       subMenu.style.maxHeight = subMenu.scrollHeight + 'px'
     })
+
+    if (item.classList.contains('main-nav-list-item--active')) {
+      mobileMenuNavSubItems.style.display = 'block'
+    }
 
     item.addEventListener('mouseleave', () => {
       mainNav.classList.remove('main-nav--non-rounded')
